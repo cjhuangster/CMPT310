9b7f8ba3c44f2c8046b628d291dfba748a909340
cjhuangster <39141143+cjhuangster@users.noreply.github.com>
2020-02-14 00:12:52 -0800

    a2_q3 works

--- aima-python-master/aima-python-master/a2_q3.py
+++ aima-python-master/aima-python-master/a2_q3.py
@@ -1,18 +1,18 @@
-from csp import*
-from a2_q1 import*
-from a2_q2 import*
+from csp import* 
+from a2_q1 import* 
+from a2_q2 import* 
+import xlsxwriter
+import time
 
-
-
-# class Icebreaker(CSP):
-friendList = a2_q1.rand_graph(100,0.2)
-# takes friend dictionary and initalizes CSP
-ibvariables = friendList.keys() #not needed
-domKeys = ibvariables
-domItems = ibvariables
-ibdomains = dict.fromkeys(domKeys, domItems)
-ibneighbors = friendList
-        
+workbook = xlsxwriter.Workbook('a2_q3.xlsx')
+worksheet = workbook.add_worksheet()
+labels = ["probability", "#teams", "run time", "#assign", "#unassign", "fun fact"]
+row = 0
+column = 0
+for item in labels:
+    worksheet.write(row,column,item)
+    column+=1
+      
 def constraints (A, a, B, b):
     # for A,B as individual:team, then check to see if a:b is ok
     #assuming B is already assigned to A, can we assume b is assigned to a?
@@ -22,52 +22,52 @@ def constraints (A, a, B, b):
             return False
         else:
             return True
-            
-    # def __init__(self, variables, domains, neighbors, constraints): 
-    #     self.variables = variables
-    #     self.domains = domains
-    #     self.neighbors = neighbors 
-    #     self.constraints = constraints  
-    #     self.curr_domains = None
-    #     self.nassigns = 0
-    
-    
-Icebreaker = CSP(ibvariables, ibdomains, ibneighbors, constraints)
 
-# print("friendList:", friendList )
-# print("ibdomains: ", ibdomains)
-# make_arc_consistent does not work without calling AC3 for whatever reason
-print("AC3: ", AC3(Icebreaker))
-# print("min_conflicts: ", min_conflicts(Icebreaker))
-# print(backtracking_search(Icebreaker))
-# for i in domKeys:
-#     print(MapColoringCSP(i, ibneighbors))
-newDomains = {}
-for i in ibdomains:
-    for j in ibdomains:
-            # print("i, j, possible team for i:", i, " ", j, "       ", make_arc_consistent(i,j,Icebreaker))
-            newItem = make_arc_consistent(i,j,Icebreaker)
-            newDomains.update({i:newItem})
-            
-print("newDomains: ", newDomains)
-print("check_teams: ", check_teams(friendList, newDomains))
-
-
-
-
-# def exactSolver(graph):
-#     # graph represents a good list of friends and bad list for teammates
-#     numPeople = len(graph)
-#     popularity = {}
-#     # get the # of good values
-#     for i in graph:
-#         popular = len(graph.get(i))
-#         popularity.update(i,popular)
+for x in range (5):
+    graphs = [rand_graph(30, 0.1), rand_graph(30, 0.2), rand_graph(30, 0.3),
+          rand_graph(30, 0.4), rand_graph(30, 0.5)]
+    # friendList = rand_graph(100,0.2)
+    for y in range(len(graphs)): 
+        startTime = time.time()
+        friendList = graphs[y]
+        # takes friend dictionary and initalizes CSP
+        ibvariables = friendList.keys() #not needed
+        domKeys = ibvariables
+        domItems = ibvariables
+        ibdomains = dict.fromkeys(domKeys, domItems)
+        ibneighbors = friendList
         
-#     # assign most popular individuals first?
+        Icebreaker = CSP(ibvariables, ibdomains, ibneighbors, constraints)
+        AC3(Icebreaker)
+        backtracking_search(Icebreaker)
+        # newDomains = {}
+        # for i in ibdomains:
+        #     for j in ibdomains:
+        #             # print("i, j, possible team for i:", i, " ", j, "       ", make_arc_consistent(i,j,Icebreaker))
+        #             newItem = make_arc_consistent(i,j,Icebreaker)
+        #             newDomains.update({i:newItem})
+        # print("ND size: ", max(newDomains.values()))
+        # print("check_teams: ", check_teams(friendList, newDomains))
+        # print("length of newDomains", len(newDomains))
+        print("check_teams backtracking: ", check_teams(friendList, backtracking_search (Icebreaker)))
+        print("backtracking_search: ", (backtracking_search(Icebreaker)))
+        teams = (backtracking_search(Icebreaker))
+        print("#team", max(teams.values()))
+        # print("bt size: ", max(backtracking_search(Icebreaker)).values())
+        print("#assigned:", Icebreaker.nassigns)
+        print("#unassigned", Icebreaker.nassigns-len(ibvariables))
+        endTime = time.time()
+        print("calculation time: ", endTime - startTime)
+        
+        teamNum = max(teams.values())
+        numAssign = Icebreaker.nassigns
+        numUnassign = Icebreaker.nassigns-len(ibvariables)
+        calcTime = endTime - startTime
+        content = [y*0.1+0.1, teamNum, calcTime, numAssign, numUnassign,  0]
+        row = x*5+y+1
+        column = 0
+        for item in content:
+            worksheet.write(row,column,item)
+            column+=1
 
-# # for testing purposes
-# print("friendList: ", friendList)
-# print("variables: ", variables)
-# print("domains: ", domains)
-# print ("neighbours: ", neighbors)
+workbook.close()  
