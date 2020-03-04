a35824466c5462c8f665aa497b6503add75c5b2b
cjhuangster <39141143+cjhuangster@users.noreply.github.com>
2020-03-03 22:17:31 -0800

    tic tac toe game works, start AI algorithm

--- a3.py
+++ a3.py
@@ -1,13 +1,15 @@
-import random  
+import random   
 
+# draws out physical representation of game
 def drawBoard(board):
-    print(board[7] + ' | ' + board[8] + ' | ' + board [9])
+    print()
+    print(board[7], ' | ', board[8], ' | ' , board [9])
     print('_____________')
-    print(board[4] + ' | ' + board[5] + ' | ' + board [6])
+    print(board[4] , ' | ' , board[5] , ' | ' , board [6])
     print('_____________')
-    print(board[1] + ' | ' + board[2] + ' | ' + board [3])
-    print('_____________')
-    
+    print(board[1] , ' | ' , board[2] , ' | ' , board [3])
+
+# gets player input on choice of letter
 def playerChoice():
     letter = ''
     while not (letter == 'X' or letter == 'O'):
@@ -19,15 +21,18 @@ def playerChoice():
         else:
             return ['O', 'X']
         
+# decides who goes first through random selection
 def whoGoesFirst():
     if random.randint(0,1) == 0:
         return 'AI'
     else:
         return 'player'
 
+# set move for players
 def move (board, letter, move):
     board[move] = letter
     
+# checks goal
 def checkWin (XorO, board):
     return ((board[1]==board[2]==board[3]==XorO) 
     or (board[4]==board[5]==board[6]==XorO)
@@ -38,10 +43,40 @@ def checkWin (XorO, board):
     or (board[1]==board[5]==board[9]==XorO)
     or (board[3]==board[5]==board[7]==XorO))
     
+# returns a list of available moves
 def availableMoves (board):
     availableSpace = []
     for i in board:
-        if board[i] == '':
+        if i != 'X' and i != 'O':
             availableSpace.append(i)
+    
+    # print(availableSpace)
     return availableSpace
 
+def aGame():
+    board = ['X',1,2,3,4,5,6,7,8,9]
+    while True:
+            aMove = random.choice(availableMoves(board))
+            # print(aMove)
+            move(board, 'X', aMove)
+            drawBoard(board)
+            
+            if checkWin ('X', board):
+                print('X won')
+                break
+            
+            if len(availableMoves(board))==0:
+                print('No one won')
+                break
+            
+            aMove = random.choice(availableMoves(board))
+            move(board, 'O', aMove)
+            drawBoard(board)
+            
+            if checkWin ('O', board):
+                print ('O won')
+            
+            if len(availableMoves(board))==0:
+                print('No one won')
+                break
+aGame()
