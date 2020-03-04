442613eddf43e008e9a6c53f4a6467c0e25bdd4b
cjhuangster <chengjie.michael.huang@gmail.com>
2020-03-04 10:21:28 -0800

    fixed repeated availablespace bug

--- a3.py
+++ a3.py
@@ -7,6 +7,7 @@ class ticTacToe:
         
     def drawBoard(self, board):
         print()
+        print()
         print(board[7], ' | ', board[8], ' | ' , board [9])
         print('_____________')
         print(board[4] , ' | ' , board[5] , ' | ' , board [6])
@@ -14,23 +15,23 @@ class ticTacToe:
         print(board[1] , ' | ' , board[2] , ' | ' , board [3])
 
     # gets player input on choice of letter
-    def playerChoice(self):
-        letter = ''
-        while not (letter == 'X' or letter == 'O'):
-            print('Do you want to be X or O?')
-            letter = input().upper()
+    # def playerChoice(self):
+    #     letter = ''
+    #     while not (letter == 'X' or letter == 'O'):
+    #         print('Do you want to be X or O?')
+    #         letter = input().upper()
             
-            if letter == 'X':
-                return ['X', 'O']
-            else:
-                return ['O', 'X']
+    #         if letter == 'X':
+    #             return ['X', 'O']
+    #         else:
+    #             return ['O', 'X']
             
-    # decides who goes first through random selection
-    def whoGoesFirst(self):
-        if random.randint(0,1) == 0:
-            return 'AI'
-        else:
-            return 'player'
+    # # decides who goes first through random selection
+    # def whoGoesFirst(self):
+    #     if random.randint(0,1) == 0:
+    #         return 'AI'
+    #     else:
+    #         return 'player'
 
     # set move for players
     def move (self, board, letter, move):
@@ -50,37 +51,43 @@ class ticTacToe:
     # returns a list of available moves
     def availableMoves (self, board):
         availableSpace = []
+        print("board: ", board)
         for i in board:
+            # bugged, returning positions that have already been filled
             if i != 'X' and i != 'O':
                 availableSpace.append(i)
         
-        # print(availableSpace)
+        print("availableSpace: ", availableSpace)
         return availableSpace
 
     def aGame(self):
         board = ['X',1,2,3,4,5,6,7,8,9]
+        count = 0
         while True:
-                aMove = random.choice(availableMoves(board))
+                aMove = random.choice(self.availableMoves(board))
                 # print(aMove)
-                move(board, 'X', aMove)
-                drawBoard(board)
-                
-                if checkWin ('X', board):
+                self.move(board, 'X', aMove)
+                self.drawBoard(board)
+                count+=1
+                print("count: ", count)
+                if self.checkWin ('X', board):
                     print('X won')
                     break
                 
-                if len(availableMoves(board))==0:
+                if len(self.availableMoves(board))==0:
                     print('No one won')
                     break
                 
-                aMove = random.choice(availableMoves(board))
-                move(board, 'O', aMove)
-                drawBoard(board)
+                aMove = random.choice(self.availableMoves(board))
+                self.move(board, 'O', aMove)
+                self.drawBoard(board)
+                count+=1
+                print("count: ", count)
                 
-                if checkWin ('O', board):
+                if self.checkWin ('O', board):
                     print ('O won')
                 
-                if len(availableMoves(board))==0:
+                if len(self.availableMoves(board))==0:
                     print('No one won')
                     break
 # recursively playout game
@@ -103,6 +110,7 @@ class pMCTS:
         copyAvailableMove = playOut.availableMoves(board)
         for i in range(numPlayOut):
             # how to avoid passing by reference of OG availableMoves?
+            # deep copy a copy of available, pop the move made in current iteration, pass to next
             aMove = random.choice(playOut.copyAvailableMove)
             if XorO == 'X':
                 nextXorO == 'O'
@@ -110,4 +118,5 @@ class pMCTS:
                 nextXorO == 'X'
             score += randomPlayOut(numPlayOut, board, nextXorO, AILetter, playerLetter)
               
-# aGame()
+newGame = ticTacToe()
+newGame.aGame()
